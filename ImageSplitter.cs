3f65b052cdaef3ac087a1f7aabb4103c7acaedbe
Vincas Turskis <vincas.turskis@gmail.com>
2026-06-02 00:06:51 +0300

    Added border shader, adjusted position of slices

--- ImageSplitter.cs
+++ ImageSplitter.cs
@@ -6,6 +6,12 @@ public partial class ImageSplitter : Node
 	private Sprite2D sprite;
 	private Sprite2D[] slices = new Sprite2D[6];
 
+	[Export]
+	private int sliceCountX;
+	[Export]
+	private int sliceCountY;
+	
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -17,21 +23,34 @@ public partial class ImageSplitter : Node
 	public void SplitImage(Image image)
 	{
 		sprite.Texture = ImageTexture.CreateFromImage(image);
+		
 		int originalWidth = image.GetUsedRect().Size.X;
 		int originalHeight = image.GetUsedRect().Size.Y;
-		int sliceWidth = originalWidth/3;
-		int sliceHeight = originalHeight/2;
-		for (int i = 0; i < 2; i++)
+
+		int globalOffsetX = 0;
+		int globalOffsetY = 200;
+
+		int sliceWidth = originalWidth/sliceCountX;
+		int sliceHeight = originalHeight/sliceCountY;
+		for (int i = 0; i < sliceCountY; i++)
 		{
-			for (int j = 0; j < 3; j++)
+			for (int j = 0; j < sliceCountX; j++)
 			{
-				int curSliceNo = (i*3) + j;
+				int newSliceWidth = sliceWidth;
+				int curSliceNo = (i*sliceCountX) + j;
 				int positionX = sliceWidth * j;
 				int positionY = sliceHeight * i;
-				Rect2I rect = new Rect2I(positionX, positionY, sliceWidth, sliceHeight);
+				int posXOffset = 0;
+				if(j == (sliceCountX - 1))
+				{
+					newSliceWidth = sliceWidth + originalWidth % sliceCountX;
+					posXOffset = originalWidth % sliceCountX / 2;
+				}
+				Rect2I rect = new Rect2I(positionX, positionY, newSliceWidth, sliceHeight);
 				Image slice = image.GetRegion(rect);
 				slices[curSliceNo] = GetNode<Sprite2D>("Slice"+(curSliceNo + 1));
 				slices[curSliceNo].Texture = ImageTexture.CreateFromImage(slice);
+				slices[curSliceNo].Position = new Vector2(positionX + posXOffset + globalOffsetX, positionY + globalOffsetY);
 			}
 		}
 	}
